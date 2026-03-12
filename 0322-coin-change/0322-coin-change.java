cd771fcc58bda17972277b32054afdf19addbd74
M-A-Mudassir <mudassirma09@gmail.com>
2026-03-12 11:20:36 +0530

    Time: 18 ms (42.22%), Space: 46.2 MB (82.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,29 +1,17 @@
 class Solution {
-    public int coinOptions(int[] coins,int amt,int[] dp){
-        if(amt == 0){
-            return 0;
-        }
-        if(amt < 0){
-            return -1;
-        }
-        if(dp[amt] != -2){
-            return dp[amt];
-        }
-        int min = Integer.MAX_VALUE;
-        for(int i:coins){
-            int curr = coinOptions(coins,amt-i,dp);
-            if(curr >= 0 && curr < min){
-                min = curr;
-            }
-            
-        }
-        dp[amt] = (min == Integer.MAX_VALUE)?-1:min+1;
-        return dp[amt];
-    }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[] dp = new int[amount+1];
-        Arrays.fill(dp,-2);
-        return coinOptions(coins,amount,dp);
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i=0;i<=amount;i++){
+            for(int j:coins){
+                if(i-j >= 0 && dp[i-j] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(1+dp[i-j],dp[i]);
+                }
+            }
+        }
+        return (dp[amount] == Integer.MAX_VALUE)?-1:dp[amount];
+
     }
 }
