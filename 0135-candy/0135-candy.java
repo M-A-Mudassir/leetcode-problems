8002285fb313d3ddfe6ee1a15cff898e2c64149e
M-A-Mudassir <mudassirma09@gmail.com>
2026-04-30 12:12:59 +0530

    Time: 3 ms (88.34%), Space: 47.8 MB (33.01%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,28 +1,26 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] larr = new int[n];
-        int[] rarr = new int[n];
-        larr[0] = 1;
-        for(int i=1;i<n;i++){
-            if(ratings[i] > ratings[i-1]){
-                larr[i] = larr[i-1]+1;
-            }else{
-                larr[i] = 1;
+        int sum =n;
+        int i=1;
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
             }
-        }
-        int past = 1;
-        int sum = 0;
-        int curr = 0;
-        sum += Math.max(past,larr[n-1]);
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i] > ratings[i+1]){
-                curr = past+1;
-            }else{
-                curr = 1;
+            int peak = 0;
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
             }
-            past = curr;
-            sum += Math.max(curr,larr[i]);
+            int down = 0;
+            while(i<n && ratings[i] < ratings[i-1]){
+                down++;
+                sum += down;
+                i++;
+            }
+            sum -= Math.min(down,peak);
         }
         return sum;
     }
