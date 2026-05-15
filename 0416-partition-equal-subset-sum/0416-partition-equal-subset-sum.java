ebdec7ba1fc18f36fb8e7952d33f73f5453feeaf
M-A-Mudassir <mudassirma09@gmail.com>
2026-05-15 16:41:58 +0530

    Time: 87 ms (25.25%), Space: 75.9 MB (13.86%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,30 +1,29 @@
 class Solution {
-    public static boolean isSumExist(int[] nums,int idx,int tar,int[][] dp){
-        if(idx == 0)return nums[0] == tar;
+    public static boolean isPoss(int[] nums,int idx,int tar,int[][] dp){
+        if(idx == 0){
+            return nums[0] == tar;
+        }
         if(dp[idx][tar] != -1){
             return dp[idx][tar] == 1;
         }
-        boolean notPick = isSumExist(nums,idx-1,tar,dp);
-        boolean pick = false;
-        if(nums[idx] <= tar){
-            pick = isSumExist(nums,idx-1,tar-nums[idx],dp);
-        }
-        dp[idx][tar] = (pick || notPick)?1:0;
-        return (pick || notPick);
+        boolean nP = isPoss(nums,idx-1,tar,dp);
+        boolean p = false;
+        if(nums[idx] <= tar)
+            p =  isPoss(nums,idx-1,tar-nums[idx],dp);
+        dp[idx][tar] = (p || nP)?1:0;
+        return (p||nP);
     }
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
+        int sum =0,n=nums.length;
         for(int i=0;i<n;i++){
             sum += nums[i];
         }
-        if(sum % 2 != 0){
+        if(sum % 2 != 0)
             return false;
-        }
-        int[][] dp = new int[n][sum/2 +1];
+        int[][] dp = new int[n][sum/2+1];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return isSumExist(nums,n-1,sum/2,dp);
+        return isPoss(nums,n-1,sum/2,dp);
     }
 }
